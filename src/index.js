be7b8455e3c58408261705d07188cc072f74855f
SnaiperLoD <snaiperlod19@gmail.com>
2018-09-23 13:56:27 +0300

    first commit 53

--- src/index.js
+++ src/index.js
@@ -4,4 +4,23 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var n=preferences.length; 
+  var count=0;
+  for(var k=0; k<n; k++)
+  {
+    var first=k+1;
+    var second=preferences[k];
+    var third=preferences[second-1];
+    if(preferences[third-1]==first)
+    {
+    count++;
+    /*if ( (k-1)>(second-1) )
+    {
+    preferences.splice((second-1),1);
+    preferences.splice((k-1),1);
+    }*/
+  }
+  }
+  return(count/3);
 };
+ 
